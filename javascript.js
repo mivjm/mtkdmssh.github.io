677c40a72454d5d1a28b8d00881124e7fad8e1de
mivjm <53676220+mivjm@users.noreply.github.com>
2023-01-24 20:42:58 +0900

    set my cord

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+/*
 document.addEventListener('DOMContentLoaded', function(){
   // オーバレイを開閉する関数
   const overlay = document.getElementById('overlay');
@@ -17,29 +17,22 @@ document.addEventListener('DOMContentLoaded', function(){
   const overlayInner = document.getElementById('overlay-inner');
   overlayInner.addEventListener('click', stopEvent, false);  
 }, false);
-/*
-document.addEventListner(
-  'DOMContentLoaded',
-  function() {
+*/
+document.addEventListner('DOMContentLoaded', function(){
     // オーバレイを解放する関数
     const overlay = document.getElementById('overlay');
     function overlayToggle() {
       overlay.classList.toggle('overlay-on');
     }
-    
     // 指定した要素に対して上記関数を実行するクリックイベントを設定
     const clickArea = document.getElementsByClassName('overlay-event');
-    for (let i=0; i<clickArea.length; i++) {
+    for (let i = 0; i < clickArea.length; i++) {
       clickArea[i].addEventListener('click', overlayToggle, false);
     }
-    
     // イベントに対してバブリングを停止
     function stopEvent(event) {
       event.stopPropagation();
     }
     const overlayInner = document.getElementById('overlay-inner');
-    overlayInner.addEventListener('click', stopEvent, false);    
-  },
-  false
-);
-*/
+    overlayInner.addEventListener('click', stopEvent, false);
+}, false);
